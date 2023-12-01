e0c785595ff9b930ba4d04c961efdee9bceb3c55
Camila Piñero <cmlpr9@gmail.com>
2023-12-01 16:43:47 -0300

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import TablaUsuarios from "./components/TablaUsuarios";
+import ModificarTablaUsuarios from "./components/ModificarTablaUsuarios";
 
 function App() {
 	return (
-		<>
+		<Router>
 			<TablaUsuarios />
-		</>
+			<Routes>
+				<Route
+					path="/modificar-usuario"
+					element={<ModificarTablaUsuarios />}
+				/>
+			</Routes>
+		</Router>
 	);
 }
 
